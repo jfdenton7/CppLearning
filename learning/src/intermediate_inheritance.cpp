05386bb00d5a66a8312ded5e0927d5c604674fc6
jfdenton <jfdenton@vt.edu>
2020-07-15 10:37:12 -0400

    adding most up to date learning exercises for cpp

--- learning/src/intermediate_inheritance.cpp
+++ learning/src/intermediate_inheritance.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+#define PI 3.145
+
 class FriendExample {
     private:
         int num;
@@ -14,8 +16,50 @@ int getOtherNum(const FriendExample& other) {
     return other.num;  // able to access because this function is a 'friend'
 }
 
+class Shape {
+    public:
+        // virtual allows polymorphism (pointer of type Shape to derived class calls the right class)
+        // =0 allows this method to become abstract
+        virtual int area() =0;
+        // ! NOTE !
+        // as an abstract base class
+        // Shape cannot be instantiated
+        
+};
+
+// inheriting from Shape
+class Circle : public Shape {
+    int radius;
+    public:
+        Circle(int r): radius(r) {}
+        int area() {    // override Shape method
+            return radius * radius * PI;
+        }
+};
+
+class Person {
+    protected: // members of subclasses can now access...
+    int age;
+    std::string name;     
+};
+
+class Student : protected Person {
+    int gradYear;
+    Student(int age, std::string name, int gradYear) {
+        this->age = age;
+        this->name = name;
+        this->gradYear = gradYear;
+    }
+};
 
 int main() {
+
+    Circle* c = new Circle(3);
+
+    Shape* s = c;
+
+    std::cout << s->area() << std::endl;
+    std::cout << c->area() << std::endl;
 
     return 0;
 }
